e6bd271ff003e40408186c8871bfb188cd86740d
CrypticiaN <obscura.darkside@gmail.com>
2022-01-01 14:42:18 +0100

    create repo, create home, controller and test

---
+++ src/main/java/org/factoriaf5/codersinff5/CodersInFf5Application.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.codersinff5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersInFf5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodersInFf5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoriaf5/codersinff5/ApplicationTests.java
@@ -0,0 +1,25 @@
+package org.factoriaf5.codersinff5;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void loadsTheHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+}
